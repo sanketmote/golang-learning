c6f71a877fe4dfaf47e0ebefeaa5109c56e57197
Sanket Mote <66875819+sanketmote@users.noreply.github.com>
2023-01-18 12:33:35 +0000

    Completed Slices and for loop

--- Cards/main.go
+++ Cards/main.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
-	fmt.Println(card)
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades"
+	// card = "Five of diamonds"
+	// card := newCard()
+	cards := []string{"Ace of Spades", newCard()}
+	cards = append(cards, "siz")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	// fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
