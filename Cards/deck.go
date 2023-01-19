59ad27c3466472355a5bc9aea270d91627393bdf
Sanket Mote <66875819+sanketmote@users.noreply.github.com>
2023-01-19 12:07:35 +0000

    Completed FIle Read and write part

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,9 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, values := range cardValues {
+			cards = append(cards, suit+"  of "+values+",")
+
+		}
+	}
+	return cards
+}
 func (d deck) print() {
 	fmt.Println(d)
+}
+
+// Multiple Return
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	s := strings.Join([]string(d), ",")
+	return s
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,17 +1,39 @@
 package main
 
+import "fmt"
+
 func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"
 	// card = "Five of diamonds"
 	// card := newCard()
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "siz")
+	// cards := deck{"Ace of Spades", newCard()}
+	// cards = append(cards, "siz")
 	// for i, card := range cards {
 	// 	fmt.Println(i, card)
 	// }
 	// fmt.Println(cards)
-	cards.print()
+
+	// cards := newDeck()
+	// // cards.print()
+
+	// // Multiple Return
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+
+	// Type Conversion from string to by byte array only  (not to int it will not work in this case)
+	// greeting := "Hi There"
+	// fmt.Println([]byte(greeting))
+	// cards := newDeck()
+	// fmt.Println(cards.toString())
+
+	//  save file
+	// fmt.Println(cards.saveToFile("sanket.txt"))
+
+	// Read File
+	fmt.Println(newDeckFromFile("sanket.txt"))
+	// fmt.Println(newDeckFromFile("sankt.txt"))
 }
 
 func newCard() string {
